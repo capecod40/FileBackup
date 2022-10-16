9982b913ca6b0bcf93f0760d1406b6246ba4155f
capecod40 <tonyzh1120@gmail.com>
2022-10-15 18:15:16 -0700

    Phase 1: Small edits

--- src/main/model/SrcFile.java
+++ src/main/model/SrcFile.java
@@ -3,7 +3,8 @@ package model;
 import java.io.File;
 
 // SrcFile class:
-// The File class contains all necessary methods for my application, so I made this
+// Java's File class contains all necessary methods for my application's user story,
+// such as copying a single file, multiple files, or a directory, so I made this
 // wrapper class to adhere to the user story guidelines:
 // "[...] there is at least one user story that involves the user adding an X to a Y
 // (e.g. adding an item to a to-do list), where X and Y are classes that you will
